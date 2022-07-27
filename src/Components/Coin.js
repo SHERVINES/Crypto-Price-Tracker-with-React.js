ef0bc558052602626bf968306d305a2a5e8296aa
shervin <shervin.ses@gmail.com>
2022-07-27 16:16:09 +0430

    some changes

--- src/Components/Coin.js
+++ src/Components/Coin.js
@@ -10,7 +10,7 @@ const Coin = ({image,name,price,priceChange,marketCap,symbol}) => {
             <span className={styles.name}>{name}</span>
             <span className={styles.symbol}>{symbol.toUpperCase()}</span>
             <span className={styles.price}>$ {price.toLocaleString()}</span>
-            <span className={priceChange > 0 ? styles.greenPriceChange : styles.redPriceChange}>{priceChange.toFixed(2)}</span>
+            <span className={priceChange > 0 ? styles.greenPriceChange : styles.redPriceChange}>{priceChange.toFixed(2)}%</span>
             <span className={styles.marketCap}>$ {marketCap.toLocaleString()}</span>
         </div>
     );
